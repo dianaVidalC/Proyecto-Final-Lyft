348405cfd33401b053f266fa89dfbdcaddecf424
Erika Vidal <diann852@gmail.com>
2017-05-02 14:44:24 -0500

    formulario

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+/*document.getElementById("toggle").addEventListener("click",function(event){
+  event.preventDefault();
+  document.getElementById("nav-header").classList.toggle("open");
+  document.getElementById("body").classList.toggle("overflow-hidden");
+});*/
+var lastScrollTop=0;
+
+window.addEventListener("scroll",function(){
+    var currentScroll=window.pageYOffset || document.body.scrolltop;
+    console.log(currentScroll);
+    if(currentScroll>lastScrollTop){
+      document.getElementById("nav-header").classList.add("header-in");
+      document.getElementById("nav-header").classList.add("solid");
+      document.getElementById("nav-header").style.opacity=1;
+      document.getElementById("sing-up").style.opacity="visibility";
+     if(currentScroll>115){
+      document.getElementById("sing-up").style.opacity="visibility";
+      }
+    }else{
+      document.getElementById("nav-header").classList.add("header-in");
+      document.getElementById("nav-header").style.opacity=1;
+    if(currentScroll<=3){
+      document.getElementById("nav-header").classList.remove("header-in");
+      document.getElementById("nav-header").classList.remove("solid");
+      document.getElementById("sing-up").style.opacity="hidden";
+    }
+  }
+  lastScrollTop=currentScroll;
+},false);
